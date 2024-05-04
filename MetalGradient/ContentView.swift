bf178ec4ae7e5fc86dbb3e63d614efde36773a56
Andrew Zheng <aheze@getfind.app>
2024-05-04 00:51:47 -0700

    Initial Commit

---
+++ MetalGradient/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  MetalGradient
+//  
+//  Created by Andrew Zheng (github.com/aheze) on 5/4/24.
+//  Copyright © 2024 Andrew Zheng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MetalGradient/MetalGradientApp.swift
@@ -0,0 +1,18 @@
+//
+//  MetalGradientApp.swift
+//  MetalGradient
+//  
+//  Created by Andrew Zheng (github.com/aheze) on 5/4/24.
+//  Copyright © 2024 Andrew Zheng. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct MetalGradientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
